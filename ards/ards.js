972dc48f6ef0dd7c6fa6e7702e9e52c36c4ec9ea
Gareth Coffey <gareth.coffey@sagepay.com>
2015-03-26 13:05:47 +0000

    Updated: ards - route paths now in line with routes/ansible.js

--- ards/ards.js
+++ ards/ards.js
@@ -1,18 +1,18 @@
 // ARDS - Ansible Remote Deployment Scheduler
- 
+
 var express = require('express');
     bodyParser = require('body-parser');
     ansible = require('./routes/ansible');
     config = require('./config.json');
     logger = require('./logger.js');
- 
+
 var App = express();
 
 App.use(bodyParser.json());
 
-App.get('/ards/bootstrap-script', ansible.bootstrapScript(file: config.ards.bootstrap_script));
-App.get('/ards/ansible-ssh-key', ansible.ansibleKey(file: config.ards.ansible_ssh_key));
+App.get('/ards/bootstrap-script', ansible.bootstrapScript);
+App.get('/ards/ansible-ssh-key', ansible.ansibleSshKey);
 App.post('/ards/add-server', ansible.addServer);
 
-App.listen(config.ards.listen_port);
+App.listen(config.ards.listen_port, '0.0.0.0');
 logger.info('ards.js - Server started on port ' + config.ards.listen_port);
